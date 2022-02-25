86ed771e4827a4b831bcaafcb3885c69a8df8572
Chad Branagan <chad.branagan@gmail.com>
2022-02-24 16:24:04 -0800

    all routes working, final commit before submit

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
-
 const express = require("express");
 const sequelize = require("./config/connection");
-// const routes = require("./routes");
+const routes = require("./routes");
 // import sequelize connection
 
 const app = express();
@@ -10,9 +9,9 @@ const PORT = process.env.PORT || 3001;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(routes);
+app.use(routes);
 
 // sync sequelize models to the database, then turn on the server
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
 });
